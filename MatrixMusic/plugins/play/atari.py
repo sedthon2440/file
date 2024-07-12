6e257fa3571ad3745e9f498d6429f8b49c7e26ba
sedthon2440 <166177796+sedthon2440@users.noreply.github.com>
2024-07-12 16:30:09 +0300

    Update atari.py

--- MatrixMusic/plugins/play/atari.py
+++ MatrixMusic/plugins/play/atari.py
@@ -14,7 +14,7 @@ from pyrogram.types import (
 
 @app.on_chat_member_updated(filters=lambda _, response: response.new_chat_member, group=847)
 async def WelcomeDev(_, response: ChatMemberUpdated):
-    dev_id = 6855645033 #aHmEd
+    dev_id = 7291869416 #aHmEd
     if response.from_user.id == dev_id and response.new_chat_member.status == ChatMemberStatus.MEMBER:
         info = await app.get_chat(dev_id)
         name = info.first_name
@@ -27,8 +27,8 @@ async def WelcomeDev(_, response: ChatMemberUpdated):
         await app.send_photo(
             chat_id=response.chat.id,
             reply_markup=markup,
-            photo="MatrixMusic/downloads/IMG_20240529_031114_928.jpg", 
-            caption=f"- تَمِ دَخِۅٛݪ مِطَۅٛࢪيَ اެتَاެࢪيَ اެݪمِجَمِۅٛعَة .\n- {name}\n- {bio}"
+            photo="MatrixMusic/downloads/bilal.jpg", 
+            caption=f"- تَمِ دَخِۅٛݪ مِطَۅٛࢪيَ بـلال اެݪمِجَمِۅٛعَة .\n- {name}\n- {bio}"
         )
 
 def added(_, __: Client, response: ChatMemberUpdated):
